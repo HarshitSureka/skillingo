0a3ea616e374ffca6d21b1d5d3e1009ac23b79e6
hsureka <hsureka@iitg.ac.in>
2023-03-08 09:34:53 +0530

    code .

---
+++ models/question.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const question = new mongoose.Schema({
+    question: {
+        type: String,
+    },
+    options: [{
+        type: String,
+    }],
+    correct_answers: [{
+        type: Number,
+    }],
+    explaination: {
+        type: String,
+    },
+    skill: {
+        type: String,
+    },
+    category: {
+        type: String,
+    },
+    sub_category: {
+        type: String,
+    },
+});
+
+module.exports = mongoose.model("Question", question);
